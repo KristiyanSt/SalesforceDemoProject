04678fcd2eda3b96af501e5c0830d03e4ea2ed39
Kristiyan Stefanov <KStefanov@nemetschek.bg>
2024-07-15 17:17:32 +0300

    implement sort options on fetching contacts

--- force-app/main/default/lwc/contactTable/contactTable.js
+++ force-app/main/default/lwc/contactTable/contactTable.js
@@ -4,89 +4,129 @@ import getContacts from "@salesforce/apex/ContactManager.getContacts";
 export default class ContactTable extends LightningElement {
     contacts;
     columns = [
-    { 
-        label: 'Name',
-        fieldName: 'Name',
-        sortable: true,
-    },
-    {
-        label: 'Phone',
-        fieldName: 'Phone',
-        type: 'number',
-        sortable: true,
-    },
-    { 
-      label: 'Email',
-      fieldName: 'Email',
-      type: 'email',
-      sortable: true
-    },
-    {
-      label: 'Created Date',
-      fieldName: 'CreatedDate',
-      type: 'date'
-    },
-    { 
-      label: 'Account',
-      fieldName: 'AccountName',
-      type: 'text',
-      sortable: true
-    },
-    { 
-      label: 'Is special',
-      fieldName: 'IsSpecial',
-      type: 'text' 
-    },
-    
+        {
+            label: 'Name',
+            fieldName: 'Name',
+            type: 'text'
+        },
+        {
+            label: 'Phone',
+            fieldName: 'Phone',
+            type: 'number'
+        },
+        {
+            label: 'Email',
+            fieldName: 'Email',
+            type: 'email'
+        },
+        {
+            label: 'Created Date',
+            fieldName: 'CreatedDate',
+            type: 'date'
+        },
+        {
+            label: 'Account',
+            fieldName: 'AccountName',
+            type: 'text'
+        },
+        {
+            label: 'Is special',
+            fieldName: 'IsSpecial',
+            type: 'text'
+        },
+
     ]
-    defaultSortDirection = 'asc';
-    sortDirection = 'asc';
-    sortedBy;
-    isSpecialFilter = false;
-    filterOptions = [{
+    sortParams = {
+        sortBy: null,
+        sortDirection: null
+    }
+    filterParams = {
+        isSpecial: null,
+        Name: null,
+        Phone: null,
+        Email: null,
+        AccountName: null
+    }
+    isSpecialFilter = null;
+    isSpecialOptions = [{
         label: "Is special",
         value: "isSpecial"
     }];
-    filterValue = [];
-    
-    
+    filterValues = [];
+    sortOptions = [
+        {
+            label: "Name",
+            value: "Name"
+        },
+        {
+            label: "Phone",
+            value: "Phone"
+        },
+        {
+            label: "Email",
+            value: "Email"
+        },
+        {
+            label: "Account name",
+            value: "Account.Name"
+        }
+    ];
+    sortDirectionOptions = [
+        {
+            label: "Ascending",
+            value: "ASC"
+        },
+        {
+            label: "Descending",
+            value: "DESC"
+        }
+    ]
+    async handleSortChange(event) {
+        const selectedOption = event.detail.value;
+
+        if(this.sortOptions.some(option => option.value === selectedOption)) {
+            this.sortParams.sortBy = selectedOption;
+        }
+
+        if(this.sortDirectionOptions.some(option => option.value === selectedOption)) {
+            this.sortParams.sortDirection = selectedOption;
+        }
+        await this.fetchContacts();
+    }
+    onFilterChange(event) {
+        console.log(event.target.fieldName);
+    }
+    async handleIsSpecialFilterChange(event) {
+        const fieldName = event.target.fieldName;
+        const value = event.target.value;
+
+        if(this.filterParams.hasOwnProperty(fieldName)){
+            console.log(JSON.stringify(value.isArray()))
+            if(value.isArray()){
+                this.isSpecialFilter = value.includes("isSpecial") ? true : null;
+            }else {
+                this.filterParams[event.target.fieldName] = event.target.value;
+            }
+        }
+        console.log(JSON.stringify(this.filterParams));
+        await this.fetchContacts();
+    }
+
     async fetchContacts() {
         try {
-            const contactsJsonRes = await getContacts({isSpecialFilter: this.isSpecialFilter});
+            const params = {
+                isSpecialFilter: this.isSpecialFilter,
+                sortBy: this.sortParams.sortBy,
+                sortDirection: this.sortParams.sortDirection
+            }
+            const contactsJsonRes = await getContacts(params);
             this.contacts = JSON.parse(contactsJsonRes);
         } catch (error) {
             console.log(error);
         }
     }
-    async onHandleFilter(event) {
-        this.isSpecialFilter = event.detail.value.includes("isSpecial") ? true : false;
-        await this.fetchContacts();
-    }
-    sortBy(field, reverse, primer) {
-        const key = primer
-        ? function (x) {
-            return primer(x[field]);
-        }
-        : function (x) {
-            return x[field];
-              };
-              
-              return function (a, b) {
-            a = key(a);
-            b = key(b);
-            return reverse * ((a > b) - (b > a));
-        };
-    }
-    onHandleSort(event) {
-        const { fieldName: sortedBy, sortDirection } = event.detail;
-        const cloneData = [...this.contacts];
-        
-        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
-        this.contacts = cloneData;
-        this.sortDirection = sortDirection;
-        this.sortedBy = sortedBy;
-    }
-    async connectedCallback(){
+
+    async connectedCallback() {
         await this.fetchContacts();
     }
 }
